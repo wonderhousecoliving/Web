faca9bd9973b1fac3d705dfe8fed4e68283513c6
riacosta <riacosta@gmail.com>
2025-04-08 17:05:42 +0200

    Update server port to 3001

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 3001;
+const PORT = 3000;
 
 // Serve static files from the public directory
 app.use(express.static('public'));
