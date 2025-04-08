07b325b2aebbd1fd3041ce685c0ee19833cf5b26
riacosta <riacosta@gmail.com>
2025-04-08 16:48:44 +0200

    Initial setup: Express server with Hello World webpage

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3001;
+
+// Serve static files from the public directory
+app.use(express.static('public'));
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+}); 
