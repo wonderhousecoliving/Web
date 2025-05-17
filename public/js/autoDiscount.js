a66fc87e982f1f03c7ba27e32f4f4fa00a1f120d
riacosta <riacosta@gmail.com>
2025-05-17 17:22:11 +0200

    test fix for hostaway search

--- public/js/autoDiscount.js
+++ public/js/autoDiscount.js
@@ -1,15 +1,30 @@
 // public/js/autoDiscount.js
 // Si la URL contiene 'checkout', rellena el input de descuento automáticamente
 console.log("autoDiscount.js loaded");
-document.addEventListener('DOMContentLoaded', () => {
-    console.log("autoDiscount.js loaded in DOMContentLoaded");
+function check30DaysStay(){
+    let errorMessage = document.querySelector('.stay-duration-error');
+    if (!errorMessage) {
+        errorMessage = document.createElement('div');
+        errorMessage.className = 'stay-duration-error';           
+        errorMessage.textContent = 'Minimum stay is 30 days';
+    }
+
     let elementClass ="sc-e50ef021-3 hftQFy";
     let element = document.querySelector(`.${elementClass}`);
     if (element) {
         console.log("element found");
-        element.value = "TEST01";
+        element.parentNode.insertBefore(errorMessage, element.nextSibling);
+
     } else {
         console.log("element not found");
     }
     
-}); 
+}; 
+
+
+// Iniciar la validación cuando el DOM esté listo
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', check30DaysStay);
+} else {
+    check30DaysStay();
+}
