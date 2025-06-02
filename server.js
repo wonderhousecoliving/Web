5c396f270e2de45fd99753da808d60cc1aecbfe9
riacosta <riacosta@gmail.com>
2025-06-02 10:52:36 +0200

    fix: add CORS headers for booking subdomain

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// CORS middleware
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://book.wonderhousecoliving.com');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 // Serve static files from the public directory with absolute path
 app.use(express.static(path.join(__dirname, 'public')));
 
